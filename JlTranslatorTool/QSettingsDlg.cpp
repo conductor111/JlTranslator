f14898fb37a0919a84f51c9a60481c62a0d908a4
Conductor <artem.semenov@jarillolabs.com>
2021-01-04 00:12:31 +0300

    QSettingsDlg: handling of the current input language changing; onOk handling.

--- JlTranslatorTool/QSettingsDlg.cpp
+++ JlTranslatorTool/QSettingsDlg.cpp
@@ -46,6 +46,45 @@ void QSettingsDlg::initSlotsInline()
     {
         moveListWidgetItem(ui->listWidget_LangsEnabled, ui->listWidget_LangsAvailable);
     });
+    connect(ui->comboBox_InputLang, QOverload<int>::of(&QComboBox::currentIndexChanged), this, [this](int index)
+    {
+        QVariant userData = ui->comboBox_InputLang->itemData(index);
+
+        QListWidget* listWidget = ui->listWidget_LangsAvailable;
+        auto listIndexes = listWidget->model()->match(listWidget->model()->index(0, 0), Qt::UserRole, userData);
+        if (listIndexes.isEmpty())
+        {
+            listWidget = ui->listWidget_LangsEnabled;
+            listIndexes = listWidget->model()->match(listWidget->model()->index(0, 0), Qt::UserRole, userData);
+        }
+        if (listIndexes.isEmpty())
+        {
+            return;
+        }
+
+        // remove
+        QListWidgetItem* item = listWidget->takeItem(listIndexes[0].row());
+
+        // insert
+        (m_onChangeInputLangAddCurrentToEnabled ? ui->listWidget_LangsEnabled : ui->listWidget_LangsAvailable)->addItem(m_currentInputLangItem.release());
+        m_currentInputLangItem.reset(item);
+
+        m_onChangeInputLangAddCurrentToEnabled = listWidget == ui->listWidget_LangsEnabled;
+    });
+    connect(ui->pushButton_Ok, &QPushButton::clicked, this, [this]()
+    {
+        m_settings.setValue(c_inputLangKey, ui->comboBox_InputLang->currentData().toString());
+
+        QString outputLangs;
+
+        for (auto item : ui->listWidget_LangsEnabled->findItems(QString("*"), Qt::MatchWrap | Qt::MatchWildcard))
+        {
+            outputLangs += (outputLangs.isEmpty() ? "" : c_outputLangsDelimiter) + item->data(Qt::UserRole).toString();
+        }
+        m_settings.setValue(c_outputLangsKey, outputLangs);
+
+        accept();
+    });
 }
 
 void QSettingsDlg::initControls()
@@ -61,13 +100,16 @@ void QSettingsDlg::initControls()
     int currentInputLangInd = -1;
     for (auto it : langsAvailable())
     {
+        QString langDesc = it.second + " - " + it.first;
+
         bool isCurrentInputLang = it.first == inputLang;
         if (isCurrentInputLang)
         {
             currentInputLangInd = ui->comboBox_InputLang->count();
+            m_currentInputLangItem = std::make_unique<QListWidgetItem>(langDesc);
+            m_currentInputLangItem->setData(Qt::UserRole, it.first);
         }
 
-        QString langDesc = it.second + " - " + it.first;
         ui->comboBox_InputLang->addItem(langDesc, it.first);
 
         if (isCurrentInputLang)
--- JlTranslatorTool/QSettingsDlg.h
+++ JlTranslatorTool/QSettingsDlg.h
@@ -1,6 +1,8 @@
 #ifndef QSETTINGSDLG_H
 #define QSETTINGSDLG_H
 
+#include <memory>
+
 #include <QDialog>
 #include <QSettings>
 #include <QListWidget>
@@ -20,6 +22,8 @@ public:
 private:
     Ui::QSettingsDlg *ui;
     QSettings& m_settings;
+    std::unique_ptr<QListWidgetItem> m_currentInputLangItem;
+    bool m_onChangeInputLangAddCurrentToEnabled = false;
 
     static inline const QString c_outputLangsDelimiter = ":";
     static inline const QString c_inputLangKey = "Langs/Input";
