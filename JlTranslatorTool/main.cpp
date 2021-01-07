842f977eaf8c3096b3b95f18df1773769e3032d5
Conductor <artem.semenov@jarillolabs.com>
2021-01-08 00:54:49 +0300

    JlTranslatorTool/main.cpp: needless content has been removed.

--- JlTranslatorTool/main.cpp
+++ JlTranslatorTool/main.cpp
@@ -1,31 +1,11 @@
-#include <memory>
-#include <vector>
-#include <iostream>
-
-//#include <QCoreApplication>
 #include <QApplication>
-#include <QMessageBox>
-
-#include <QUrl>
-#include <QUrlQuery>
 
 #include "MainWindow.h"
 
-#include "test.h"
-
 int main(int argc, char *argv[])
-{
-    //QCoreApplication a(argc, argv);
-    QApplication a(argc, argv);
-
-    try
-    {
-        //test();
-    }
-    catch(const std::exception &ex)
-    {
-        QMessageBox::warning(nullptr, "Error", ex.what());
-    }
+{    
+    QApplication::setAttribute(Qt::AA_DisableWindowContextHelpButton);
+    QApplication a(argc, argv);    
 
     MainWindow w;
     w.show();
